5e0d53610d2cb936469668176b09a69071bfd2ad
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-05-11 11:44:53 +0100

    changes to classes, content and styling

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-import './src/styles/index.scss';
+import "./src/styles/index.scss";
+
+const inputBox = document.querySelector(".app__container__input-box__input");
+const addBtn = document.querySelector(".app__container__input-box__button");
+const taskContainer = document.querySelector(
+  ".app__container__task-section__list"
+);
+
+function addTask() {
+  if (inputBox.value === "") {
+    alert("Please enter a task");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBox.value;
+    li.classList.add("app__container__task-section__list__item");
+    taskContainer.appendChild(li);
+    inputBox.value = "";
+  }
+}
+
+addBtn.addEventListener("click", addTask);

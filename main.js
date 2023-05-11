53ec835bae3b947cf981d307444f165c8a134442
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-05-11 13:08:11 +0100

    modified code to render nested li elements on function call

--- main.js
+++ main.js
@@ -11,8 +11,35 @@ function addTask() {
     alert("Please enter a task");
   } else {
     let li = document.createElement("li");
-    li.innerHTML = inputBox.value;
     li.classList.add("app__container__task-section__list__item");
+
+    let leftContent = document.createElement("div");
+    leftContent.classList.add(
+      "app__container__task-section__list__item__left-content"
+    );
+
+    let leftContentIcon = document.createElement("img");
+    leftContentIcon.classList.add(
+      "app__container__task-section__list__item__left-content__icon"
+    );
+    leftContentIcon.src = "/unchecked.svg";
+
+    let leftContentText = document.createElement("span");
+    leftContentText.classList.add(
+      "app__container__task-section__list__item__left-content__text"
+    );
+    leftContentText.innerHTML = inputBox.value;
+
+    let rightContentIcon = document.createElement("img");
+    rightContentIcon.classList.add(
+      "app__container__task-section__list__item__right-content-icon"
+    );
+    rightContentIcon.src = "/close.svg";
+
+    leftContent.appendChild(leftContentIcon);
+    leftContent.appendChild(leftContentText);
+    li.appendChild(leftContent);
+    li.appendChild(rightContentIcon);
     taskContainer.appendChild(li);
     inputBox.value = "";
   }
